300958d0fa1d8cd2a8ad5f2e9f66d876db444183
Martin Rey <m.rey00@gmail.com>
2026-04-01 17:44:43 -0300

    Revert "Cambios nuevos para el revert."
    This reverts commit 80744ea8c3ff628d1bff418d14e7d77f799f386c.
    
    En la rama main
    Cambios a ser confirmados:
    	borrados:        .vscode/changelists.json
    	modificados:     main.py

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 def main():
     print("Hello from is3-clase-versionado-26!")
 
-def unused_function:
-    print("This function is not used in the main flow of the program.")
 
 if __name__ == "__main__":
     main()
