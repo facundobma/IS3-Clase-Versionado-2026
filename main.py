f8cb12595c6a59f05c7f4a47b52ab8ed80698abf
Martin Rey <m.rey00@gmail.com>
2026-04-01 17:43:35 -0300

    Cambios nuevos para el revert.

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 def main():
     print("Hello from is3-clase-versionado-26!")
 
+def unused_function:
+    print("This function is not used in the main flow of the program.")
 
 if __name__ == "__main__":
     main()
