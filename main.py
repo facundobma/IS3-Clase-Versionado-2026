215d86453ea0b60d1fef531d05312d7db08673a7
Martín Rey <tinxo@users.noreply.github.com>
2026-04-08 16:47:38 -0300

    Soy Cosme Fulanito hago un cambio en main.py para incorporar un método de setup.

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 def main():
     print("Hello from is3-clase-versionado-26!")
+    env = setup()
+    print(f"Trabajando en entorno: {env}")
+
+def setup():
+    env = 'dev'
+    return env
 
 
 if __name__ == "__main__":
