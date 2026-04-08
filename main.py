0f2542da56d19d2ffabb6641112dc9aec4a45000
Martin Rey <m.rey00@gmail.com>
2026-04-08 16:55:22 -0300

    Inicio de trabajo en la feature de operaciones matemáticas.

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
+def suma(a, b):
+    return a + b
+
+
 def main():
     print("Hello from is3-clase-versionado-26!")
     env = setup()
     print(f"Trabajando en entorno: {env}")
+    result = suma(5, 3)
+    print(f"La suma de 5 y 3 es: {result}")
+
 
 def setup():
     env = 'dev'
