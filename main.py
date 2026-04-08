8249918ecba1d8ce1bf7274657c99ac083234983
Martin Rey <m.rey00@gmail.com>
2026-04-08 17:01:53 -0300

    Feature terminada. Lista para merge con dev.

--- main.py
+++ main.py
@@ -20,8 +20,21 @@ def main():
     print("Hello from is3-clase-versionado-26!")
     env = setup()
     print(f"Trabajando en entorno: {env}")
-    result = suma(5, 3)
-    print(f"La suma de 5 y 3 es: {result}")
+    operacion = "X"
+    print("Selecciona una operación: +, -, *, /")
+    operacion = input("Ingrese la operación: ") 
+    if operacion == "+":
+        result = suma(5, 3)
+        print(f"La suma de 5 y 3 es: {result}")
+    elif operacion == "-":
+        result = resta(5, 3)
+        print(f"La resta de 5 y 3 es: {result}")
+    elif operacion == "*":
+        result = multiplicacion(5, 3)
+        print(f"La multiplicación de 5 y 3 es: {result}")
+    elif operacion == "/":  
+        result = division(5, 3)
+        print(f"La división de 5 y 3 es: {result}")
 
 
 def setup():
